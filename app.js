09ceef381a06159b47b7e9eb27bc73d00bef62fd
LencyR <lencyrivera216912@gmail.com>
2024-03-06 11:49:08 +0800

    added server side rendering dynamic data and simple css

--- app.js
+++ app.js
@@ -12,20 +12,26 @@ app.listen(3000);
 app.get('/', (req, res) => {
     //res.send('<p>home page</p>');
     //res.sendFile('./views/index.html', { root: __dirname })
-    res.render('index');
+    const blogs = [
+        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectur'},
+        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectur'},
+        {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectur'},
+    ];
+    
+    res.render('index', { title: 'Home', blogs });
 });
 
 app.get('/about', (req, res) => {
     //res.send('<p>about page</p>');
     //res.sendFile('./views/about.html', { root: __dirname })
-    res.render('about');
+    res.render('about', { title: 'About' });
 });
 
 app.get('/blogs/create', (req, res) => {
-    res.render('create');
+    res.render('create', { title: 'Create a new Blog' });
 })
 
 // 404 page
 app.use((req, res) => {
-    res.status(404).render('404');
+    res.status(404).render('404', { title: '404' });
 });
