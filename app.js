69f66f01450aec5c9e53e318450209d8eeff93dd
LencyR <lencyrivera216912@gmail.com>
2024-03-06 14:58:19 +0800

    added mongodb functionalities using mongoose

--- app.js
+++ app.js
@@ -1,29 +1,31 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
 
 // express app
 const app = express();
 
+// connect to mongodb
+const dbURI = 'mongodb+srv://userone:nvKTypUNRr68Xmxr@nodelearn.oj7yghx.mongodb.net/nodelearn?retryWrites=true&w=majority&appName=nodelearn'
+mongoose.connect(dbURI)
+    .then((result) => app.listen(3000))
+    .catch((err) => console.log(err));
+
 // register view engine
 app.set('view engine', 'ejs');
-
-// listen for requests
-app.listen(3000);
 
 // middleware & static files
 app.use(express.static('public'));
 app.use(morgan('dev'));
 
+
+
+// routes
 app.get('/', (req, res) => {
     //res.send('<p>home page</p>');
     //res.sendFile('./views/index.html', { root: __dirname })
-    const blogs = [
-        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectur'},
-        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectur'},
-        {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectur'},
-    ];
-    
-    res.render('index', { title: 'Home', blogs });
+    res.redirect('/blogs')
 });
 
 app.get('/about', (req, res) => {
@@ -31,6 +33,17 @@ app.get('/about', (req, res) => {
     //res.sendFile('./views/about.html', { root: __dirname })
     res.render('about', { title: 'About' });
 });
+
+// blog routes
+app.get('/blogs', (req, res) => {
+    Blog.find().sort({ createdAt: -1 })
+        .then((result) => {
+            res.render('index', { title: 'All Blogs', blogs: result })
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+})
 
 app.get('/blogs/create', (req, res) => {
     res.render('create', { title: 'Create a new Blog' });
