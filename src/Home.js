f7ee0c3e91f36fe09ed8ff9a4d5d42913ab59edd
LencyR <lencyrivera216912@gmail.com>
2024-03-10 13:45:05 +0800

    adding functions to props

--- src/Home.js
+++ src/Home.js
@@ -7,13 +7,19 @@ const Home = () => {
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ]);
+
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlogs);
+    }
+
     const handleClick = (name) => {
         
     }
 
     return (
         <div className="home">
-            <BlogList blogs={blogs} title='All Blogs' />
+            <BlogList blogs={blogs} title='All Blogs' handleDelete={handleDelete} />
         </div>
     );
 }
